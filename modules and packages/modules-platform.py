5d47e42d6318c6cb3b40cb84d477306a86dae530
PaulP <PaulP>
2025-02-19 10:00:17 +0000

    added platfomr module

---
+++ modules and packages/modules-platform.py
@@ -0,0 +1,8 @@
+import platform
+
+print("Plaform of this system is: " + platform.platform())
+print("CPU platform of this system is: " + platform.machine())
+print("OS of this system is: " + platform.system())
+print("Processor of this system is: " + platform.processor())
+print("Python implementation of this system is: " + platform.python_implementation())
+print("Python version of this system is: ", platform.python_version_tuple())
